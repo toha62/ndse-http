37a58bda8fecee218856521a255928ab51e7d24e
Anton Trifonov <toha62@inbox.ru>
2023-07-09 16:20:56 +0300

    add formatting output

---
+++ config.js
@@ -0,0 +1,4 @@
+const UNITS = 'm';
+const LANGUAGE = 'ru'; // does not support in subscribe "Free"
+
+module.exports = { UNITS, LANGUAGE };
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const readline = require('node:readline');
 const { stdin: input, stdout: output } = require('node:process');
+const { UNITS, LANGUAGE } = require('./config');
 
 const getCity = () => { 
   return new Promise((resolve, reject) => {
@@ -21,8 +22,8 @@ const getCity = () => {
 
 const getData = (city) => {
   const accessKey = process.env.AccessKey;
-  const url = `http://api.weatherstack.com/current?access_key=${accessKey}&query=${city}`;
-
+  const url = `http://api.weatherstack.com/current?access_key=${accessKey}&query=${city}&units=${UNITS}`;
+  
   return new Promise((resolve, reject) => {
     http.get(url, (res) => {
       const {statusCode} = res;
@@ -53,7 +54,11 @@ const start = async () => {
     const city = await getCity();
     const data = await getData(city);
 
-    console.log(data);
+    console.log(`Forecast for: ${data.location.name}, ${data.location.country}`);
+    console.log(`Current temperature: ${data.current.temperature} ${UNITS === 'm' ? 'Celsius' : UNITS === 's' ? 'Kelvin' : 'Fahrenheit'}`);
+    console.log(`Current wind: ${data.current.wind_dir} ${data.current.wind_speed} ${UNITS === 'f' ? 'Miles/hour' : 'Kilometers/Hour'}`);
+    console.log(`Current humidity: ${data.current.humidity}%`);
+    console.log(`Current pressure: ${data.current.pressure} Millibar`);
   } catch (err) {
     console.error(err);
   } finally {
